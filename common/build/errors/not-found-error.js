91772fa9ae42858108ec578dd506f2d7973d3e43
summer <nut22_ks@hotmail.com>
2024-08-05 10:52:51 +0700

    Updates

---
+++ common/build/errors/not-found-error.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NotFoundError = void 0;
+const custom_error_1 = require("./custom-error");
+class NotFoundError extends custom_error_1.CustomError {
+    constructor(message) {
+        super(message);
+        this.message = message;
+        this.statusCode = 404;
+        Object.setPrototypeOf(this, NotFoundError.prototype);
+    }
+    serializeErrors() {
+        return [{ message: this.message }];
+    }
+}
+exports.NotFoundError = NotFoundError;
---
+++ common/build/errors/request-validation-error.d.ts
@@ -0,0 +1,14 @@
+import { CustomError } from "./custom-error";
+import { ValidationError } from 'express-validator';
+export declare class RequestValidationError extends CustomError {
+    errors: ValidationError[];
+    statusCode: number;
+    constructor(errors: ValidationError[]);
+    serializeErrors(): ({
+        message: any;
+        field: string;
+    } | {
+        message: any;
+        field?: undefined;
+    })[];
+}
