bc86630c982a0a216ddb697fda3fa3466676b04c
summer <nut22_ks@hotmail.com>
2024-08-06 13:14:18 +0700

    add ingress-service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,11 +3,19 @@ import userDb from "./db/user";
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
-userDb.public.one(`
-    CREATE TABLE "user" ("id" SERIAL NOT NULL, "uuid" text NOT NULL, "email" text NOT NULL, "username" text NOT NULL, "password" text NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"));
-    `);
+// userDb.public.one(`
+//     CREATE TABLE "user" ("id" SERIAL NOT NULL, "uuid" text NOT NULL, "email" text NOT NULL, "username" text NOT NULL, "password" text NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"));
+//     `);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`App listening at port ${PORT}`);
-});
+const start = async () => {
+    userDb.connect(() => {
+        console.log('Db is connected');
+    });
+
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`App listening at port ${PORT}`);
+    });
+}
+
+start();
